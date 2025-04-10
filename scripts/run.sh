09951a739d168e705cb423fb0323164a6d774d05
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-10 11:29:50 +0530

    final commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-
 set -e
 
-# Simple wait script
-echo 'Waiting for database to be ready...'
-while ! python -c 'import MySQLdb; MySQLdb.connect(host="$DB_HOST", user="$DB_USER", password="$DB_PASS", database="$DB_NAME")' 2>/dev/null; do
-  echo 'Database not ready yet, waiting...'
-  sleep 1
-done
-echo 'Database is ready!'
+# Wait for database
+./wait-for-db.sh
 
+# Run migrations and collect static files
 python manage.py collectstatic --noinput
 python manage.py migrate
 
+# Start the application
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
